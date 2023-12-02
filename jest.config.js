e194b4303ede465a2560368ecb5ede5ff28a9700
jonathan-gartland <jonathan-gartland@gmail.com>
2023-12-02 13:37:52 -0500

    added playwright tests to ignore path for jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
-const nextJest = require("next/jest");
+const nextJest = require('next/jest');
 const createJestConfig = nextJest({
-  dir: "./",
+  dir: './',
 });
 const customJestConfig = {
-  moduleDirectories: ["node_modules", "<rootDir>/"],
-  testEnvironment: "jsdom",
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jsdom',
+  modulePathIgnorePatterns: ['tests/e2e'],
 };
 module.exports = createJestConfig(customJestConfig);
