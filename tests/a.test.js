765d0f6d444dc173124495d739db00f52edf435e
jonathan-gartland <jonathan-gartland@gmail.com>
2023-12-02 11:25:42 -0500

    set up prettier to manage format

--- tests/a.test.js
+++ tests/a.test.js
@@ -1,10 +1,10 @@
 // import Page from "../src/app/page";
-import "@testing-library/jest-dom";
+import '@testing-library/jest-dom';
 // import { fireEvent, render, screen } from "@testing-library/react";
-import { expect, describe, test } from "@jest/globals";
+import { expect, describe, test } from '@jest/globals';
 
-describe("adding a passing test to project", () => {
-  test("passes", async () => {
+describe('adding a passing test to project', () => {
+  test('passes', async () => {
     await expect(1).toEqual(1);
   });
 });
