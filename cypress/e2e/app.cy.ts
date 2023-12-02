419e91b893e0e5687cbfce1b5f3942459a4aca44
jonathan-gartland <jonathan-gartland@gmail.com>
2023-12-02 17:16:43 -0500

    added basic passing cypres test

---
+++ cypress/e2e/app.cy.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+// Disable ESLint to prevent failing linting inside the Next.js repo.
+// If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
+// https://github.com/cypress-io/eslint-plugin-cypress
+
+// Cypress E2E Test
+describe('Navigation', () => {
+
+  it('passes this test', () => {
+    expect(1).equal(1);
+  });
+
+  it('passes this test too', () => {
+    expect(2).equal(2);
+  });
+
+});
+
+// Prevent TypeScript from reading file as legacy script
+export {}
