c3e24cafda2dc6f53462e4a14e0b6a83aa58a860
jonathan-gartland <jonathan-gartland@gmail.com>
2023-12-02 19:19:31 -0500

    cypress implemented, stub tests work, holding hewre for now

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,17 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  },
+
+  component: {
+    devServer: {
+      framework: 'next',
+      bundler: 'webpack',
+    },
+    specPattern: 'cypress/**/*.cy.{js,jsx,ts,tsx}',
   },
 });
---
+++ cypress/e2e/components/component.cy.js
@@ -0,0 +1,8 @@
+// import React, { View } from 'react';
+// import { mount } from 'cypress/react'
+
+describe('a test', () => {
+  it('passes', () => {
+    // cy.mount(<View />);
+  });
+});
