dd36ac413e0932d5daeb7f054012d93ad81d71e5
jonathan-gartland <jonathan-gartland@gmail.com>
2023-12-06 13:48:14 -0500

    component tests converted to cypress

--- cypress/components/page.cy.js
+++ cypress/components/page.cy.js
@@ -1,31 +1,13 @@
 import Page from '../../src/app/page';
-// import '@testing-library/jest-dom';
-//import { /*fireEvent,*/ render, screen } from '@testing-library/react';
-// import { expect, describe, test, beforeAll } from '@jest/globals';
 
 describe('adding a passing test to project', () => {
-  // beforeAll(() => {});
-  it('passes', async () => {
-    await expect(1).eq(1);
-  });
+  // before(() => {});
 
-  // it('page test', async () => {
-  //   render(<Page />);
-  //   expect(screen.getByTestId('next-testid')).to.be.
-  // });
-  //
-  // it('page test', async () => {
-  //   render(<Page />);
-  //   expect(screen.getByTestId('next-image-testid')).toBeInTheDocument();
-  // });
-  //
-  // it('page test', async () => {
-  //   render(<Page />);
-  //   expect(screen.getByText('Get started by editing')).toBeInTheDocument();
-  // });
-  //
-  // it('page test', async () => {
-  //   render(<Page />);
-  //   expect(screen.getByText('Deploy')).toBeInTheDocument();
-  // });
+  it('Check text in page, quick validation', () => {
+    cy.mount(<Page />);
+    cy.get('#cyid').then(($cyid) => {
+      expect($cyid).to.contain('Learn about Next.js');
+    });
+  });
+  // after(() => {});
 });
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -30,7 +30,7 @@ export default function Home() {
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
-          data-testid={'next-image-testid'}
+          data-cy={'next-image-cy'}
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
           alt="Next.js Logo"
@@ -41,7 +41,8 @@ export default function Home() {
       </div>
 
       <div
-        data-testid={'next-testid'}
+        id={'cyid'}
+        data-cy={'next-div-cy'}
         className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"
       >
         <a
@@ -60,7 +61,6 @@ export default function Home() {
             Find in-depth information about Next.js features and API.
           </p>
         </a>
-
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
@@ -77,7 +77,6 @@ export default function Home() {
             Learn about Next.js in an interactive course with&nbsp;quizzes!
           </p>
         </a>
-
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -94,7 +93,6 @@ export default function Home() {
             Explore starter templates for Next.js.
           </p>
         </a>
-
         <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
