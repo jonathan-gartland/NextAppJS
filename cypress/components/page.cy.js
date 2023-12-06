511d37e867af333704be44eacb75918c7b1154e9
jonathan-gartland <jonathan-gartland@gmail.com>
2023-12-06 18:52:46 -0500

    save state

--- cypress/components/page.cy.js
+++ cypress/components/page.cy.js
@@ -5,9 +5,15 @@ describe('adding a passing test to project', () => {
 
   it('Check text in page, quick validation', () => {
     cy.mount(<Page />);
-    cy.get('#cyid').then(($cyid) => {
-      expect($cyid).to.contain('Learn about Next.js');
-    });
+    cy.get('[data-cy="next-div-cy"]').should(
+      'contain.text',
+      'Learn about Next.js'
+    );
+
+    //  alternate way if needed
+    //  cy.get('#cyid').then(($cyid) => {
+    //    expect($cyid).to.contain('Learn about Next.js');
+    //  }); // can chain finally, etc if need be
   });
   // after(() => {});
 });
